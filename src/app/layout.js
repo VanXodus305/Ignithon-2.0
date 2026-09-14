f3e90ced8be19553c5d1bab3ddf8475e427470bb
VanXodus305 <soumyadeep.30505@gmail.com>
2026-09-14 12:48:23 +0530

    Initial project setup

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Ignithon 2.0 OC Portal",
+  description: "A portal for the Ignithon 2.0 Organizing Committee to manage and track event-related tasks and information.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
